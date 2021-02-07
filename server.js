819dc98748c16a15076929ac74e9615563ea489a
Gitanjit <gitanjit6@gmail.com>
2021-02-07 18:33:57 +0530

    forms

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const { reqAdminAuth } = require('./middleware/authAdmin');
 
+const authController = require('./controllers/authController');
 
 const authRoutes = require('./routes/authRoutes');
 
@@ -18,23 +19,28 @@ const questions = require('./models/questions')
 const comp = require('./models/companies')
 const exp = require('./models/blog')
 
- // middleware
- //app.use(express.static('public'));  //  assets and/or public
- app.use(express.json());
- app.use(cookieParser());
 
 const top= require('./models/topics')
 const topicquesRouter=require('./routes/topques')
 const experienceRouter=require('./routes/experience')
 const app = express()
-app.use('/assets',express.static('assets'))
 
+ // middleware
+ app.use(express.static('public'));
+ app.use(express.json());
+ app.use(cookieParser());
+
+ //view engine 
+ app.set('view engine', 'ejs')
+
+
+//db connection
 const PORT = process.env.PORT || 4040
 mongoose.connect('mongodb://localhost/interview', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/interview'
-app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
@@ -43,13 +49,10 @@ app.use(methodOverride('_method'))
 
  app.get('*',checkUser);
 
- app.get('/admin', reqAdminAuth);
+ app.get('/admin');
+
 
 app.get('/', async (req, res) => {
-  
-  res.render('home.ejs')
-})
-app.get('/top', async (req, res) => {
   const topics = await top.find().sort({ createdAt: 'desc' })
   res.render('topques/index', { topics: topics })
 })
@@ -58,10 +61,15 @@ app.get('/exp', async (req, res) => {
   const companies = await comp.find().sort({ createdAt: 'desc' })
   res.render('experience/index', { companies : companies })
 })
+app.get('/form',  authController.form_get);
+app.post('/form',authController.form_post);
+//app.get('/form_blog',  authController.form_blog_get);
+//app.post('/form_blog',authController.form_blog_post);
 
 
   app.use('/topques', topicquesRouter)
   app.use('/experience', experienceRouter)
+  
 //admin bro
 
 AdminBro.registerAdapter(AdminBroMongoose)
@@ -72,7 +80,10 @@ AdminBro.registerAdapter(AdminBroMongoose)
   const router = AdminBroExpress.buildRouter(adminBro)
   app.use(adminBro.options.rootPath, router)
   
+  
+
   app.use(authRoutes);
+
 
 
 
