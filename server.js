85f61e810bd72526c53895b6324a82b27130f0e0
Sarthak Bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-02-21 23:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const storage = multer.diskStorage({
  },
   
     filename: function(req, file, cb) {
-      cb(null, new Date().toISOString() + file.originalname);
+      cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
     }
   });
   
